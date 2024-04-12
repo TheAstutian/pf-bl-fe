a9b5f36046e974ced808c8f37bb8a942d9a7400c
TheAstutian <speakingatoms@gmail.com>
2024-04-12 21:14:46 +0100

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <div className='title'>
+        <span className='main-title'>Emmanuel.</span>
+        <span className='sub-title'>Thoughts, opinions, rants.</span>
+      </div>
+      <div className='home-body'>
+        List of blog items
+      </div>
+    </div>
   )
 }
 
