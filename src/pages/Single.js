a44f7b176c62b84d5b5041bd660cc63821dbe357
TheAstutian <speakingatoms@gmail.com>
2024-04-14 11:06:38 +0100

    structuring single page

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div>
+      <div>Blog Title</div>
+      <div><img src="https://www.photographyblog.com/imager/entryimages/126961/canon_eos_r50_photos_fdf3792588d24390107925a7a9d408d3.webp"/></div>
+      <div>
+        <p>Lorem ipsum</p>
+      </div>
+    </div>
   )
 }
 
