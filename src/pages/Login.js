f48afa51c35e728cffac4b9358a07068fec8e725
TheAstutian <speakingatoms@gmail.com>
2024-04-18 22:09:19 +0100

    login page setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
 
 const Login = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    password:""
+  })
+
+  const handleChange=e=>{
+    setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit= async e =>{
+    e.preventDefault()
+    console.log(inputs)
+  }
+
+
   return (
-    <div>Login</div>
+    <div className='auth'>
+      <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="username" name="username" onChange={handleChange}/>
+        <input type="password" placeholder="password" name="password"onChange={handleChange}/>
+        <button onClick={handleSubmit}>Login</button>
+      </form>
+
+    </div>
   )
 }
 
