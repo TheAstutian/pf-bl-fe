7fa06b6cfa84fd36f4d7520c2c671679638f6b55
TheAstutian <speakingatoms@gmail.com>
2024-04-22 09:51:18 +0100

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import Single from './Single.js'
 
 const Home = () => {
   return (
     <div className='home-container'>
       <div className='title'>
-        <span className='main-title'>Emmanuel.</span>
-        <span className='sub-title'>Thoughts, opinions, rants.</span>
+        <h1 className='main-title'>The Astutian.</h1>
+        <p className='sub-title'>Thoughts, opinions, rants.</p>
       </div>
       <div className='home-body'>
-        List of blog items
+        <hr/>
+        <span>tags, tags, tags</span>
+        <h2>TItle Title Title</h2>
+        <hr/>
       </div>
     </div>
   )
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username:"",
+    password:""
+  })
+
+  const handleChange=e=>{
+    setInputs(prev=>({...prev, [e.target.name]:e.target.value}))
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault()
+    console.log(inputs)
+  }
+
   return (
-    <div>Register</div>
+   <div className='auth'>
+    <h1>Register</h1>
+    <form>
+      <input required type="text" placeholder="username" name="username" onChange={handleChange}/>
+      <input required type="password" placeholder="password" name="password" onChange={handleChange}/>
+      <button onClick={handleSubmit}> Register</button>
+    </form>
+   </div>
   )
 }
 
